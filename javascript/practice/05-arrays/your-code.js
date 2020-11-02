a1a2bfd9e9401623eefdce3aa7bda8ae4489df7a
Doyley1 <jdoyley@live.com>
2020-11-02 10:44:57 +0000

    Javascript practice 5 completed

--- javascript/practice/05-arrays/your-code.js
+++ javascript/practice/05-arrays/your-code.js
@@ -9,20 +9,27 @@
 // Tip: Start with 2 numbers in the array. Once the function works with 2 numbers, add a third, and so on.
 // Write your code here:
 
+function findMax (array) {
+    var max = array[0];
+    for (var position = 1; position < array.length; position++) {
+        max = Math.max(max, array[position]);
+    }
+    return max;
+}
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
 
 // Uncomment the code below to verify your function is working.
-// console.log("findMax =====");
-// var numbers = [46, 3, 40, 74, 63];
-// var actual = findMax(numbers);
-// console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
-// numbers = [3, 3, 3, 3];
-// actual = findMax(numbers);
-// console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
-// numbers = [-1];
-// actual = findMax(numbers);
-// console.log("[-1] -> expected: -1, actual: %s", actual);
+console.log("findMax =====");
+var numbers = [46, 3, 40, 74, 63];
+var actual = findMax(numbers);
+console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
+numbers = [3, 3, 3, 3];
+actual = findMax(numbers);
+console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
+numbers = [-1];
+actual = findMax(numbers);
+console.log("[-1] -> expected: -1, actual: %s", actual);
 
 
 // 2. COMBINE ARRAYS
@@ -32,16 +39,18 @@
 // The function should NOT alter either parameter.
 // Write your code here:
 
-
+function combine(array1, array2) {
+    return array1.concat(array2);
+}
 
 // Uncomment the code below to verify your function is working.
-// console.log("combine =====");
-// var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
-// console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
-// merged = combine([12.5, 3, 855], [true, false, true]);
-// console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
-// merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
-// console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
+console.log("combine =====");
+var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
+console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
+merged = combine([12.5, 3, 855], [true, false, true]);
+console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
+merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
+console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
 
 
 // 3. CHALLENGE: SUBTRACT AN ARRAY
@@ -53,16 +62,26 @@
 // The function should NOT alter either parameter.
 // Write your code here:
 
-
+function subtractArray(array1, array2) {
+    var array3 = array1;
+    for(var i = 0; i<=array1.length - 1; i++) {
+        for(var n = 0; n <= array2.length - 1; n++) {
+            if (array1[i] == array2[n]) {
+                array3.splice(i,1)
+            } 
+        }
+    }
+    return array3;   
+}
 
 // Uncomment the code below to verify your function is working.
-// console.log("subtractArray =====");
-// var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
-// console.log("expected: 1,3,5\nactual: %s", result);
-// result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
-// console.log("expected: Neptune,Uranus,Saturn\nactual: %s", result);
-// result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
-// console.log("expected: 2,2,2\nactual: %s", result);
+console.log("subtractArray =====");
+var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
+console.log("expected: 1,3,5\nactual: %s", result);
+result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
+console.log("expected: Neptune,Uranus,Saturn\nactual: %s", result);
+result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
+console.log("expected: 2,2,2\nactual: %s", result);
 
 
 
